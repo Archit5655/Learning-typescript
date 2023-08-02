e6454b0f9224bed34e08d9b8e2d9abc04b0bb443
Archit <architgargagra@gmail.com>
2023-08-03 02:53:02 +0530

    get to know about tuples and enum

---
+++ basics/enum.ts
@@ -0,0 +1,7 @@
+const enum Seatchoice {
+    lower="Lower",middle="middle",upper="upper",window="Window"
+}
+const seat =Seatchoice.lower
+console.log(seat)
+// seat=Seatchoice.window'
+export{}
--- basics/tuples.ts
+++ basics/tuples.ts
@@ -1,7 +1,22 @@
 
+// const user:(string | number)[]=["hsc",1]
+// in this we initialize a tuple in a specific format of data  input
+let tuser:[string,number,boolean]
 
 
+// right format
+tuser=["shdcvsd",234,true]
+// wrong format of data input
+// tuser=[23,true,"dsf"]
 
+let rgb:[number,number,number]=[23,34,340]
+type user=[number,string]
+const newuser:user=[2323,"cadc@adv"]
+// In ts we can change the value of tuples
+
+newuser[1]="aisuhfuiew@sakdbcoa"
+
+newuser.push(true)
 
 
 export{}
