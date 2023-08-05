74114af0722c0e5bdd091972cb5af4e7441853a2
Archit <architgargagra@gmail.com>
2023-08-05 19:26:23 +0530

    learned about gettes and setters and acesss modify

--- Purets/.dist/index.js
+++ Purets/.dist/index.js
@@ -1,11 +1,39 @@
 "use strict";
 // calsses in ts
-//  use commapn tsc -w to watch ts file run 
+//  use commapn tsc -w to watch ts file run
+// class User {
+//  public email: string;
+//   name: string;
+// //   accesss specifier public and private and on eother
+// private  readonly city: string = "Agra";
+//   constructor(email: string, name: string) {
+//     this.email = email;
+//     this.name = name;
+//   }
+// }
 class User {
-    constructor(email, name) {
-        this.city = "Agra";
+    constructor(email, name, userid) {
         this.email = email;
         this.name = name;
+        this.userid = userid;
+        this._courseno = 23;
+    }
+    // private method can be accessed in the class only 
+    deletetoken() {
+        console.log("token deleted");
+    }
+    //   getters and setters
+    get getemaial() {
+        return ` email is ${this.email}`;
+    }
+    get courseno() {
+        return this._courseno;
+    }
+    set courseno(course) {
+        if (course <= 1) {
+            throw new Error("Courseno should be ,more than 1");
+        }
+        this._courseno = course;
     }
 }
-const hitesh = new User("Architrdc@sdvd", "Hitesh");
+const hitesh = new User("Architrdc@sdvd", "Hitesh", "2ewds");
--- Purets/src/index.ts
+++ Purets/src/index.ts
@@ -1,13 +1,43 @@
 // calsses in ts
-//  use commapn tsc -w to watch ts file run 
-class User{
-    email:string
-    name:string
-   readonly city:string="Agra"
+//  use commapn tsc -w to watch ts file run
+// class User {
+//  public email: string;
+//   name: string;
+// //   accesss specifier public and private and on eother
+// private  readonly city: string = "Agra";
 
-    constructor(email:string,name:string){
-        this.email=email;
-        this.name=name
+//   constructor(email: string, name: string) {
+//     this.email = email;
+//     this.name = name;
+//   }
+// }
+class User {
+  private _courseno = 23;
+  constructor(
+    public email: string,
+    public name: string,
+    private userid: string
+  ) {
+   
+  }
+  // private method can be accessed in the class only 
+  private deletetoken() {
+    console.log("token deleted")
+    
+  }
+  //   getters and setters
+  get getemaial(): string {
+    return ` email is ${this.email}`;
+  }
+
+  get courseno(): number {
+    return this._courseno;
+  }
+  set courseno(course) {
+    if (course <= 1) {
+      throw new Error("Courseno should be ,more than 1");
     }
+    this._courseno = course;
+  }
 }
-const hitesh=new User("Architrdc@sdvd","Hitesh")
+const hitesh = new User("Architrdc@sdvd", "Hitesh", "2ewds");
