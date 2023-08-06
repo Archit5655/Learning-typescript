8518d1a2a73a46c50f5dd6d296c663538693f077
Archit <architgargagra@gmail.com>
2023-08-06 20:04:28 +0530

    almost done with ts

--- Purets/src/detection.ts
+++ Purets/src/detection.ts
@@ -80,3 +80,52 @@ function getfood(pet: Fish | Fly){
 
 }
 
+
+// discriminated union
+
+
+interface circle{
+    kind: "circle",
+    radius:number,
+
+}
+
+interface square{
+    kind: "square"
+    side:number
+}
+
+interface rectangle{
+    kind:"rectangle",
+    length:number,
+    width:number,
+}
+
+type Shape =circle |square |rectangle
+function getshape(shape:Shape){
+    if(shape.kind ==="circle"){
+        return Math.PI *shape.radius**2
+
+    }
+// return shape.side * shape.side
+}
+
+function getArea(shape:Shape){
+    switch (shape.kind){
+        case "circle":
+           return  Math.PI *shape.radius**2
+
+           case "square":
+            return  shape.side * shape.side
+            case "rectangle":
+                return shape.length*shape.width
+
+            default :
+            const shapedefault:never= shape
+            return shapedefault
+
+
+            }
+}
+
+
